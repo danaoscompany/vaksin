54e040f5ff82e7b9348fe8bd98ba752ab2ebb11f
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-02 17:16:21 +0700

    a

---
+++ application/controllers/Main.php
@@ -0,0 +1,57 @@
+<?php
+
+class Main extends CI_Controller {
+  
+  public function execute() {
+    $cmd = $this->input->post('cmd');
+    $this->db->query($cmd);
+    //echo json_encode($this->db->display_errors());
+  }
+  
+  public function query() {
+    $cmd = $this->input->post('cmd');
+    echo json_encode($this->db->query($cmd)->result_array());
+  }
+  
+  public function get_user_by_email_password() {
+    echo 1;
+  }
+  
+  public function get() {
+		$name = $this->input->post('name');
+		echo json_encode($this->db->get($name)->result_array());
+	}
+	
+	public function get_by_id() {
+		$name = $this->input->post('name');
+		$id = intval($this->input->post('id'));
+		echo json_encode($this->db->get_where($name, array(
+			'id' => $id
+		))->result_array());
+	}
+	
+	public function get_by_id_name() {
+		$name = $this->input->post('name');
+		$idName = $this->input->post('id_name');
+		$id = intval($this->input->post('id'));
+		echo json_encode($this->db->get_where($name, array(
+			$idName => $id
+		))->result_array());
+	}
+	
+	public function get_by_id_name_string() {
+		$name = $this->input->post('name');
+		$idName = $this->input->post('id_name');
+		$id = $this->input->post('id');
+		echo json_encode($this->db->get_where($name, array(
+			$idName => $id
+		))->result_array());
+	}
+	
+	public function delete_by_id() {
+    $name = $this->input->post('name');
+    $id = intval($this->input->post('id'));
+    $this->db->where('id', $id);
+    $this->db->delete($name);
+  }
+}
