189c2fdb14ea1f3b5b4d6fda6d1ced3c86668bbd
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-02 17:38:25 +0700

    a

---
+++ application/controllers/User.php
@@ -0,0 +1,55 @@
+<?php
+
+class User extends CI_Controller {
+  
+  public function signup() {
+    $email = $this->input->post('email');
+    $password = $this->input->post('password');
+    $users = $this->db->get_where('users', array(
+      'email' => $email
+    ))->result_array();
+    if (sizeof($users) > 0) {
+      echo json_encode(array(
+        'response_code' => -1
+      ));
+    } else {
+      $this->db->insert('users', array(
+        'email' => $email,
+        'password' => $password
+      ));
+      $lastID = intval($this->db->insert_id());
+      $user = $this->db->get_where('users', array(
+        'id' => $lastID
+      ))->row_array();
+      echo json_encode(array(
+        'response_code' => 1,
+        'data' => $user
+      ));
+    }
+  }
+  
+  public function login() {
+    $email = $this->input->post('email');
+    $password = $this->input->post('password');
+    $users = $this->db->get_where('users', array(
+      'email' => $email
+    ))->result_array();
+    if (sizeof($users) > 0) {
+      $user = $users[$i];
+      if ($user['password'] == $password) {
+        echo json_encode(array(
+          'response_code' => 1,
+          'user_id' => intval($user['id'])
+        ));
+      } else {
+        echo json_encode(array(
+          'response_code' => -1
+        ));
+      }
+    } else {
+      echo json_encode(array(
+        'response_code' => -2
+      ));
+    }
+  }
+}
