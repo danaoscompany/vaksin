4349318186151d22f440fe9f6cb661ba1ae1b296
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-06 11:51:44 +0700

    a

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,6 +2,40 @@
 
 class Admin extends CI_Controller {
   
+  public function edit_article() {
+    $articleID = intval($this->input->post('article_id'));
+    $title = $this->input->post('title');
+    $content = $this->input->post('content');
+    $imageChanged = intval($this->input->post('image_changed'));
+    if ($imageChanged == 1) {
+      $config = array(
+        'upload_path' => './userdata',
+        //'allowed_types' => "mp4|avi|ogg|flv|wmv|3gp",
+        'allowed_types' => "*",
+        'overwrite' => TRUE,
+        'max_size' => "2097152"
+        );
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+    if($this->upload->do_upload('file')) {
+      $this->db->where('id', $articleID);
+      $this->db->update('articles', array(
+        'title' => $title,
+        'content' => $content,
+        'img' => $this->upload->data()['file_name']
+      ));
+    } else {
+      echo json_encode($this->upload->display_errors());
+    }
+    } else {
+      $this->db->where('id', $articleID);
+      $this->db->update('articles', array(
+        'title' => $title,
+        'content' => $content
+      ));
+    }
+  }
+  
   public function add_article() {
     $title = $this->input->post('title');
     $content = $this->input->post('content');
