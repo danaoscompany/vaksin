30ef395658d1e49a6cd728eaa01a87cd25c15984
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-07 09:55:27 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -243,6 +243,7 @@ $this->email->send();
     $slotID = intval($this->input->post('slot_id'));
     $noAnggota = 1;
     $usedVaccines = $this->db->query("SELECT * FROM `used_vaccines` WHERE `user_id`=" . $userID . " AND `slot_id`=" . $slotID . " ORDER BY `no_anggota` DESC LIMIT 1")->result_array();
+    echo json_encode($usedVaccines);
     if (sizeof($usedVaccines) > 0) {
       $noAnggota = $usedVaccines[0]['no_anggota'];
       $noAnggota = intval($noAnggota)+1;
