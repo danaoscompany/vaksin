683c7b67aef6828162b54a3332b33421c192e023
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-12 14:42:53 +0700

    a

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,6 +4,25 @@ require('Message.php');
 
 class Admin extends CI_Controller {
   
+  public function accept_user_chat() {
+    $userID = intval($this->input->post('user_id'));
+    $adminID = intval($this->input->post('admin_id'));
+    $chatWithAdminID = intval($this->db->get_where('users', array(
+      'id' => $userID
+    ))->row_array()['chat_with_admin_id']);
+    if ($chatWithAdminID == 0) {
+      $this->db->where('id', $userID);
+      $this->db->update('users', array(
+        'chat_with_admin_id' => $adminID
+      ));
+      $this->db->query("DELETE FROM `messages` WHERE `user_id`=" . $userID . " AND `admin_id`=" . $adminID . " AND `type`=1");
+      echo 1;
+    } else {
+      $this->db->query("DELETE FROM `messages` WHERE `user_id`=" . $userID . " AND `admin_id`=" . $adminID . " AND `type`=1");
+      echo -1;
+    }
+  }
+  
   public function send_message() {
     $message = $this->input->post('message');
     $shortMessage = $message;
