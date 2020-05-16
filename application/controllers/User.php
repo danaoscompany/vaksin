06f76d472df5bce96a9a6c9b967247b58a160367
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-16 11:00:45 +0700

    a

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,11 +18,11 @@ class User extends CI_Controller {
   
   public function remind_vaccine() {
     date_default_timezone_set('Asia/Jakarta');
-    $currentDate = strtotime(date('Y:m:d H:i:s'));
+    $currentDate = DateTime::createFromFormat('Y:m:d h:i:s', date('Y:m:d H:i:s'));
     $users = $this->db->get('users')->result_array();
     for ($i=0; $i<sizeof($users); $i++) {
       $user = $users[$i];
-      $birthDate = strtotime($user['birthday']);
+      $birthDate = DateTime::createFromFormat('Y:m:d H:i:s', $user['birthday']);
       $diff = $currentDate->diff($birthDate);
       echo "Month difference: " . $diff->m . "<br>";
       $timelines = $this->db->get_where('timeline', array(
