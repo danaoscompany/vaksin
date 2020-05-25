5d165df371bb23ab0098cf2461b9542bd65a994d
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-05-25 07:56:10 +0700

    New changes

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -390,7 +390,35 @@ class User extends CI_Controller {
   
   public function get_added_vaccines() {
     $userID = intval($this->input->post('user_id'));
-    $results = $this->db->query("SELECT * FROM `used_vaccines` WHERE `user_id`=" . $userID . "")->result_array();
+    $results = $this->db->query("SELECT * FROM `used_vaccines` WHERE `user_id`=" . $userID . " AND `done`=0")->result_array();
+    for ($i=0; $i<sizeof($results); $i++) {
+      $row = $results[$i];
+      $slot = $this->db->get_where('slots', array(
+        'id' => intval($row['slot_id'])
+      ))->row_array();
+      $results[$i]['start_date'] = $slot['start_date'];
+      $results[$i]['end_date'] = $slot['end_date'];
+      $results[$i]['slots'] = $slot['slots'];
+      $results[$i]['slots_used'] = $slot['slots_used'];
+      /*$vaccineNameID = intval($this->db->get_where('vaccines', array(
+        'id' => intval($slot['vaccine_id'])
+        ))->row_array()['vaccine_name_id']);
+      $results[$i]['vaccine_name'] = $this->db->get_where('vaccine_names', array(
+        'id' => $vaccineNameID
+      ))->row_array()['name'];
+      $vaccineTypeID = intval($this->db->get_where('vaccines', array(
+        'id' => intval($slot['vaccine_id'])
+        ))->row_array()['vaccine_type_id']);
+      $results[$i]['vaccine_type'] = $this->db->get_where('vaccine_types', array(
+        'id' => $vaccineNameID
+      ))->row_array()['name'];*/
+    }
+    echo json_encode($results);
+  }
+  
+  public function get_finished_vaccines() {
+    $userID = intval($this->input->post('user_id'));
+    $results = $this->db->query("SELECT * FROM `used_vaccines` WHERE `user_id`=" . $userID . " AND `done`=1")->result_array();
     for ($i=0; $i<sizeof($results); $i++) {
       $row = $results[$i];
       $slot = $this->db->get_where('slots', array(
