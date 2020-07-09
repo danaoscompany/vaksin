581f51ab49ceec3dd555b091cbe3bcb7c141110e
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-07-09 12:24:18 +0700

    Last commit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,9 +13,8 @@ class Admin extends CI_Controller {
       $slot = $this->db->get('slots')->row_array();
       if ($slot['start_date'] >= $date && $date < $slot['end_date']) {
         $user = $this->db->get_where('users', array('id' => intval($slots[$i]['user_id'])))->row_array();
-      	$activeSlot = array(
-      	  'name' => $user['name']
-      	);
+      	$activeSlot = $slots[$i];
+      	$activeSlot['name'] = $user['name'];
       	array_push($activeSlots, $activeSlot);
       }
     }
