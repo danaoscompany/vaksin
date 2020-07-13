4dcbec3abb28df53f6ad4c846fc9ed145aab2285
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-07-13 13:34:13 +0700

    Last commit

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -1,15 +1,43 @@
 <?php
 
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 class Test extends CI_Controller {
 	
 	public function email() {
-		$this->load->library('email');
-		$this->email
-    		->from('admin@rumahvaksin.com', 'RumahVaksin')
-    		->to('danaoscompany@gmail.com')
-    		->subject('Hello from Example Inc.')
-    		->message('Hello, We are <strong>Example Inc.</strong>')
-    		->set_mailtype('html');
-		$this->email->send();
+		$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'mail.rumahvaksincikarang.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = '_mainaccount@rumahvaksincikarang.com';                     // SMTP username
+    $mail->Password   = 'V4ks1n2020';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('_mainaccount@rumahvaksincikarang.com', 'Mailer');
+    $mail->addAddress('danaoscompany@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addReplyTo('_mainaccount@rumahvaksincikarang.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 	}
 }
