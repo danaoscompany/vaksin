fc413fc88ccaf937d6724f03b27a1965687ab8dd
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-07-13 13:48:55 +0700

    Last commit

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -14,14 +14,14 @@ class Test extends CI_Controller {
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = 2;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'mail.rumahvaksincikarang.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'localhost';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = '_mainaccount@rumahvaksincikarang.com';                     // SMTP username
     $mail->Password   = 'V4ks1n2020';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('_mainaccount@rumahvaksincikarang.com', 'Mailer');
